1f96c47024ab13f98e3c9bba285f2df22fd15600
Ozan Yıldız <developer@yildizozan.com>
2020-03-24 15:08:10 +0000

    Update

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ var io = require('socket.io')(http);
 //   pingTimeout: 60000,
 // });
 
+const BROKER_CONN = process.env.BROKER_CONN;
+
 // Websocket part
 io.on('connection', function (socket) {
     console.log('a user connected');
@@ -13,36 +15,36 @@ io.on('connection', function (socket) {
         console.log('user disconnected');
     });
 
-    amqp.connect('amqp://rabbitmq', function (error0, connection) {
+    amqp.connect(BROKER_CONN, function (error0, connection) {
         if (error0) {
             throw error0;
         }
+
         connection.createChannel(function (error1, channel) {
             if (error1) {
                 throw error1;
             }
-            var exchange = 'logs';
 
-            channel.assertExchange(exchange, 'fanout', {
-                durable: false
-            });
+            var exchange = 'conveyer';
 
-            channel.assertQueue('', {
-                exclusive: true
-            }, function (error2, q) {
+            channel.assertExchange(exchange, 'fanout', { durable: true });
+
+            channel.assertQueue('clients', {}, function (error2, q) {
                 if (error2) {
                     throw error2;
                 }
+
                 console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
                 channel.bindQueue(q.queue, exchange, '');
 
                 channel.consume(q.queue, function (msg) {
-                    if (msg.content) {
+                    if (msg !== null) {
                         console.log(" [x] %s", msg.content.toString());
                         socket.emit('new data', msg.content.toString());
+                        
+                        // Send acknowledge to broker
+                        channel.ack(msg);
                     }
-                }, {
-                    noAck: true
                 });
             });
         });
